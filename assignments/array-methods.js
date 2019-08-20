ace7785c4a995d3d6863981f4ad575a38d78625b
Chrismis79 <chrismis79@gmail.com>
2019-08-20 16:51:21 -0500

    array methods 1 & 2 done

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -57,13 +57,19 @@ const runners = [
 
 // ==== Challenge 1: Use .forEach() ====
 // The event director needs both the first and last names of each runner for their running bibs. Combine both the first and last names and populate a new array called `fullNames`. This array will contain just strings.
-let fullNames = [];
+let fullNames = runners.map((item) =>{
+  return `${item.first_name} ${item.last_name}`
+});
 console.log(fullNames);
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runners' first names in uppercase because the director BECAME DRUNK WITH POWER. Populate an array called `firstNamesAllCaps`. This array will contain just strings.
-let firstNamesAllCaps = [];
-console.log(firstNamesAllCaps);
+let allCaps = runners.map((item) => {
+  return item.first_name.toUpperCase();
+});
+console.log(allCaps);
+
+
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue. We need a filtered version of the runners array, containing only those runners with large sized shirts so they can choose a different size. This will be an array of objects.
@@ -78,7 +84,11 @@ console.log(ticketPriceTotal);
 // ==== Challenge 5: Be Creative ====
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many of the array methods listed above.
 
-// Problem 1
+// Problem 1 //log first name and email
+let nameEmail = runners.map((item) =>{
+  return `${item.first_name} email is ${item.email}`;
+})
+console.log(nameEmail);
 
 // Problem 2
 
